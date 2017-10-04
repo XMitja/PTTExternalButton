03884f643c286fedb57d55d213edf49617961df6
mitja <mitja.sarp@gmail.com>
2017-10-04 14:33:31 +0100

    test button

--- app/src/main/java/fi/wt/pttexternalbutton/MainActivity.kt
+++ app/src/main/java/fi/wt/pttexternalbutton/MainActivity.kt
@@ -1,14 +1,19 @@
 package fi.wt.pttexternalbutton
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isTalking = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,24 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        button.setOnClickListener { view ->
+            isTalking = !isTalking
+            setButtonState()
+        }
+        setButtonState()
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun setButtonState() {
+        Log.d("State: ", isTalking.toString())
+        if(isTalking) {
+            button.text = "Talking"
+            sendOrderedBroadcast(Intent("android.intent.action.PTT_BUTTON_DOWN"), null)
+        }else{
+            button.text = "Not Talking"
+            sendOrderedBroadcast(Intent("android.intent.action.PTT_BUTTON_UP"), null)
         }
     }
 
